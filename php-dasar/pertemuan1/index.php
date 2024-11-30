1810b47ae1236a3653c629f215100a2460ab843c
FaqihAdzikry <faqihbelajarid@gmail.com>
2024-11-30 19:46:33 +0700

    Add files via upload

---
+++ php-dasar/pertemuan1/index.php
@@ -0,0 +1,18 @@
+<?php
+//echo "adzikry studio";
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=<, initial-scale=1.0">
+    <title>pertemuan 1</title>
+    <link rel="stylesheet" href="style.css"/>
+</head>
+<body>
+    <div class="sy">
+    <h1>PERTEMUAN 1 PHP DASAR</h1>
+    <P> membahas lingkungan pengembangan dasar PHP</p>
+</div>
+</body>
+</html>
