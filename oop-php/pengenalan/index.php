6ae7d43aa20ed6cfa42e9097c073f777ba367a20
FaqihAdzikry <faqihbelajarid@gmail.com>
2024-12-24 08:10:19 +0700

    Add files via upload

---
+++ oop-php/pengenalan/index.php
@@ -0,0 +1,60 @@
+<?php
+OBJECT ORIENTED PROGRAMMING(OOP)
+adalah pemrogramman berbasis objek.
+
+
+PERBEDAAN PROCEDURAL PROGRAMMING DAN OOP
+PP =
+    KARAKTERISTIK
+        1. instrusksi dioakukan langkah demi langkah.
+        2. memecah program menjadi bagian bagian yang kecil.
+        3. disebut prosedur, subroutine atau function.
+        4. linear atau top to bottom.
+        5. digunakan oleh bahasa pemrograman Fortn, ALGOL, COBOL, Pascal, C, php, Javascript, dll.
+    
+    KELEBIHAN
+        1. to the point.
+        2. simplicity dan kemudahan implementasi
+        3. mudah ditelusuri.
+        4. membutuhkan lebih sedikit memory jika dibandingkan dengan OOP.
+
+OOP = 
+    KARAKTERISTIK
+        1. menyusun semua kode program dan struktur data sebagai objek.
+        2. objek adalah unit dasar dari program.
+        3. objek menyimpan data dan perilaku.
+        4. objek bisa saling berinteraksi.
+        5. digunakan oleh bahasa pemrograman java, Ruby, Phyton, Javascript, PHP 5+, dll.
+
+    KELEBIHAN
+        1. representasi duni nyata.
+        2. enkapsulasi dan abstraksi data.
+        3. reusability.
+        4. skalabilitas dan ekstenbilitas.
+        5. kemudahan pengelolaan.
+        6. kolaborasi.
+        7. digunakan oleh framework.
+
+KONSEP OOP PADA PHP
+
+    BASIC = 1. class dan object.
+            2. property dan method.
+            3. contstruktor.
+            4. object type.
+            5. inheritance.
+            6.visibility(access modifier).
+            7. setter dan getter.
+            8. static method
+
+    ADVANCED = 1. abstract and interface.
+                2. interceptor.
+                3. object cloning.
+                4. callbacks and closueres.
+                5.namespace and auto loading.
+                6. dll.
+                
+
+
+
+
+?>
