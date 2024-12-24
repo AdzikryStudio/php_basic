fda35873a5731eb4e29157d1b69d1225bbea842b
FaqihAdzikry <faqihbelajarid@gmail.com>
2024-12-24 08:29:53 +0700

    Add files via upload

---
+++ oop-php/pertemuan1/index.php
@@ -0,0 +1,28 @@
+<?php
+// CLASS AND OBJECT.
+// class = 1. template atau bueprint untuk membuat instance sebuah objek.
+//         2. class mendefinisikan object.  
+//         3. menyimpan data dan perilaku yang disebut dengan property dan method. 
+    
+//     membuat class = 1. diawali class, diikuti nama dan dintasi dengan {} untuk menyoimpan property dan
+//                         method
+//                     2. aturan penaamaan class sama seperti variable. 
+
+                //   class coba { 
+                //     public $a; //property
+
+                //     //method
+                //     public function b(){
+                        
+                //     }
+
+                //   }  
+
+
+// object = 1. instance yang didefinisikan oleh class. 
+//             2. banyak object yang didefinisikan oleh satu class. 
+//             3. object dibuat dengan menggunakan keyword new. 
+
+            //   $a = new coba();
+            //   $b = new coba(); 
+?>
---
+++ oop-php/pertemuan1/produk.php
@@ -0,0 +1,12 @@
+<?php
+
+//jual produk
+//hp
+//laptop
+
+class Produk{
+
+}
+
+$produk1 = new Produk();
+$produk2 = new Produk();
