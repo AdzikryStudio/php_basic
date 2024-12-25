001aa5cb81b6e1869b752c8d4b0cf47ebb72b4d2
FaqihAdzikry <faqihbelajarid@gmail.com>
2024-12-25 12:45:04 +0700

    Add files via upload

---
+++ oop-php/pertemuan2/index.php
@@ -0,0 +1,22 @@
+<?php
+
+// PROPERTY AND METHOD
+// A.  PROPERTY 
+//     1. mempresentasikan data atau keadaan dari sebuah objek.
+//     2. variabel yang ada di dalam objek atau member variabel.
+//     3. sama seperti variabel, ditamah dengan visibility
+//         visibility = public, private, protected.
+
+// B. METHOD
+//     1. merepresantikn perilaku dari sebuah object. 
+//     2. function yang ada di dalam object. 
+//     3. sama seperti function, ditamah dengan visibility
+//         visibility = public, private, protected.
+
+// contoh kasus : 
+// property = nama, merk, warna, kecepatanmakx,jmlpenumpang. (data)
+// method = pluskecepatan, minkecepatan, gantitransmisi, belok (perilaku)
+
+
+
+
