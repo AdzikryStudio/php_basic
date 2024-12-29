46c0380dd5f5d618d8b93d18f5135ba691b8a14a
FaqihAdzikry <faqihbelajarid@gmail.com>
2024-12-29 09:18:28 +0700

    Add files via upload

---
+++ oop-php/pertemuan9/index.php
@@ -0,0 +1,11 @@
+<?php
+// VISIBILITY
+//     - konsep yang digunakan untuk mengatur akses dari properrty and method pada sebuah objek
+//     - keyword visiility = public, private, dan protected.
+    // - public = dapat digunakan di mana saja, bahkan da luar class.
+    // - protected = hanaya dapat digunakan di dalam sebuah elas beserta turunannya.
+    // - private =  hanya dapat digunakan di dalam sebuah kelas tertentu saja
+
+// - hanya memperlihatkan aspek dari class yang dibutuhkan oleh klieb.
+// - mementukaan kebutuhan yang jelas untuk object
+// - memerikan kendali pada kode untuk menghindari bug,
